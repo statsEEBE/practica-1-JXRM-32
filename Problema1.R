20a146f93a9e1434deecd433b9263483e43338f1
josep.xavier.ricart <josep.xavier.ricart@auleseebe.upc.edu>
2024-09-12 10:48:55 +0200

    Faig tres vectors amb colors i formes diferents

--- Problema1.R
+++ Problema1.R
@@ -1,5 +1,6 @@
 #Codigo para problema 1
 
+#Faig tres vectors amb colors i formes diferents
 plot(1:5, col="red",pch=3)
-plot(5:1, col="blue",pch=16)
-plot(5:1, col="yellow",pch=100)
+plot(4:2, col="blue",pch=16)
+plot(8:10, col="yellow",pch=100)
